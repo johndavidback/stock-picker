a3f5938e8a7ecff9a5b31d94efd2d97c831f13c4
John Back <johndavidback@gmail.com>
2014-05-08 14:18:30 -0400

    Adding in some volatility and things

--- stocks.py
+++ stocks.py
@@ -2,6 +2,7 @@
 
 import sys
 from pprint import pprint
+from datetime import datetime
 
 import ystockquote
 from dateutil.relativedelta import relativedelta
@@ -13,7 +14,9 @@ for quote in sys.argv[1:]:
     # First thing we want is some basic information on the quote.
     details = ystockquote.get_all(quote)
 
-    print 'Last Open: $%s' % details.get('today_open')
+    last_trade = float(ystockquote.get_last_trade_price(str(quote)))
+
+    print 'Last Open: $%s' % details.get('today_open'), 'Last Trade: $%s' % last_trade
     print 'Today\'s Change: %s' % ystockquote.get_todays_value_change(quote).split(' ')[2].rstrip('"')
 
     days = 70
@@ -21,7 +24,7 @@ for quote in sys.argv[1:]:
     today = datetime.today().strftime("%Y-%m-%d")
     fifty_ago = (datetime.today() - relativedelta(days=days)).strftime("%Y-%m-%d")
 
-    prices = ystockquote.get_historical_prices(str(arg), fifty_ago, today)
+    prices = ystockquote.get_historical_prices(str(quote), fifty_ago, today)
 
     total = 0
     for d, p in prices.iteritems():
@@ -31,8 +34,7 @@ for quote in sys.argv[1:]:
     volatility = round(total / len(prices), 4) * 100
     optional = ' !!! ' if volatility >= 3.0 else u''
 
-    last_trade = float(ystockquote.get_last_trade_price(str(arg)))
-    print '%s: %s%% ($%s) %s' % (arg, volatility, last_trade, optional)
+    print 'Volatility: %s%% %s' % (volatility, optional)
 
 
     print ''
