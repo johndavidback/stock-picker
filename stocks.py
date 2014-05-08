80ab4363e2288f1e6a7dd902a7ffe4f4416a3e35
John Back <johndavidback@gmail.com>
2014-05-08 11:02:06 -0400

    Adding in the updated requirements, oops

--- stocks.py
+++ stocks.py
@@ -16,9 +16,23 @@ for quote in sys.argv[1:]:
     print 'Last Open: $%s' % details.get('today_open')
     print 'Today\'s Change: %s' % ystockquote.get_todays_value_change(quote).split(' ')[2].rstrip('"')
 
+    days = 70
 
+    today = datetime.today().strftime("%Y-%m-%d")
+    fifty_ago = (datetime.today() - relativedelta(days=days)).strftime("%Y-%m-%d")
 
+    prices = ystockquote.get_historical_prices(str(arg), fifty_ago, today)
 
+    total = 0
+    for d, p in prices.iteritems():
+        numerator = float(p.get('High')) - float(p.get('Low'))
+        total += numerator / float(p.get('Open'))
+
+    volatility = round(total / len(prices), 4) * 100
+    optional = ' !!! ' if volatility >= 3.0 else u''
+
+    last_trade = float(ystockquote.get_last_trade_price(str(arg)))
+    print '%s: %s%% ($%s) %s' % (arg, volatility, last_trade, optional)
 
 
     print ''
