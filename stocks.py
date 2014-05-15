297c63c45cb44d8381ec2b8da160d2a179782f9a
John Back <johndavidback@gmail.com>
2014-05-14 21:38:41 -0400

    NOw it should be a bit smarter, only run when it's from the command line

--- stocks.py
+++ stocks.py
@@ -72,115 +72,120 @@ def get_sp_500_list():
 
     return symbols
 
-hopefuls = []
-hopefuls_mind_pe = []
-undervalued_stocks = []
-ignore_dividend_stocks = []
+def process():
+    hopefuls = []
+    hopefuls_mind_pe = []
+    undervalued_stocks = []
+    ignore_dividend_stocks = []
 
-if sys.argv[1] == '500':
-    quote_list = get_sp_500_list()
-else:
-    quote_list = sys.argv[1:]
-
-for quote in quote_list:
-    # Let's set up some basic criteria to see if this is a stock that
-    # warrants further inspection. These will be boolean markers we'll flip on 
-    # if it makes sense.
-    DIVIDEND_YIELD = False  # Let's say we want this >= %1
-    DESIRED_DIVIDEND_RATE = 1
-
-    # Relative Intrinsic value
-    RIV_RATIO = False  # Let's say we want this > 1.00
-    DESIRED_RIV_RATE = 1
-
-    VOLATILITY = False  # Let's say we want this > 2.0%
-    DESIRED_VOLATILITY_RATE = 2
-
-    PE_RATIO = False  # We want it at 15 or less.
-    DESIRED_PE_RATIO = 15
-
-    AAA_BOND_RATE = 3.96
-
-    print 'Finding quote for %s' % quote
-
-    # First thing we want is some basic information on the quote.
-    details = ystockquote.get_by_id_list(quote, [
-        'p',  # 0 Previous close
-        'l1',  # 1 Last trade price
-        'o',  # 2 Today's open
-        'w1',  # 3 Today's value change
-        'e7',  # 4 EPS - Current year
-        'e9',  # 5 EPS - Next year
-        'r',  # 6 PE ratio
-        'y',  # 7 Dividend yield
-    ])
-
-    # Set up some variables that are used throughout
-    last_trade = float(details[1])
-
-    print 'Prev Close: %s' % details[0], 'Today Open: $%s' % details[2], 'Last Trade: $%s' % last_trade
-    print 'Today\'s Change: %s' % details[3].split(' ')[2].rstrip('"')
-
-    # Get the past ~50 BIZ days of trading.
-    volatility = get_volatility(quote)
-    print 'Volatility: %s%%' % (volatility)
-    VOLATILITY = True if volatility > DESIRED_VOLATILITY_RATE else False
-
-    # Get Ben grahams formula
-    try:
-        eps = float(details[4])
-    except ValueError:
-        # Likely returned N/A
-        eps = 0
-    try:
-        next_eps = float(details[5])
-    except ValueError:
-        next_eps = 0
-
-    # IV stands for Intrinsic Value.  What the stock is possibly really worth.
-    iv = ben_graham(eps, next_eps, AAA_BOND_RATE)
-
-    if iv != 0:
-        # Get the relative intrinsic value, RIV
-        riv = round(iv / last_trade, 4)
-        print 'RIV: %s' % riv  # We want RIV to be better than 1.00, which would mean it's undervalued.
-        RIV_RATIO = True if riv > DESIRED_RIV_RATE else False
+    if sys.argv[1] == '500':
+        quote_list = get_sp_500_list()
     else:
-        riv = 0
+        quote_list = sys.argv[1:]
 
-    # Add to our list if applicable
-    if riv > 1.0:
-        undervalued_stocks.append(quote)
+    for quote in quote_list:
+        # Let's set up some basic criteria to see if this is a stock that
+        # warrants further inspection. These will be boolean markers we'll flip on 
+        # if it makes sense.
+        DIVIDEND_YIELD = False  # Let's say we want this >= %1
+        DESIRED_DIVIDEND_RATE = 1
 
-    # Check out the PE ratio.
-    try:
-        pe = float(details[6])
-        print 'P/E ratio: %s' % pe
-        PE_RATIO = True if pe <= DESIRED_PE_RATIO else False
-    except ValueError:
-        pe = 'Check Research'
-        pass
+        # Relative Intrinsic value
+        RIV_RATIO = False  # Let's say we want this > 1.00
+        DESIRED_RIV_RATE = 1
 
-    # Check out the dividends on this babe
-    try:
-        div_yield = float(details[7])
-        print 'Dividend yield: %s%%' % div_yield
-        DIVIDEND_YIELD = True if div_yield >= DESIRED_DIVIDEND_RATE else False 
-    except ValueError:
-        print 'Dividend yield: %s' % details[7]
+        VOLATILITY = False  # Let's say we want this > 2.0%
+        DESIRED_VOLATILITY_RATE = 2
 
-    if DIVIDEND_YIELD and RIV_RATIO and VOLATILITY and PE_RATIO:
-        print '***** Check out: %s *****' % quote
-        hopefuls.append(quote)
-    elif DIVIDEND_YIELD and RIV_RATIO and VOLATILITY:
-        print '***** Check out: %s, mind the PE (%s) *****' % (quote, pe)
-        hopefuls_mind_pe.append(quote)
-    elif RIV_RATIO and VOLATILITY:
-        ignore_dividend_stocks.append(quote)
+        PE_RATIO = False  # We want it at 15 or less.
+        DESIRED_PE_RATIO = 15
 
-    print ''
+        AAA_BOND_RATE = 3.96
 
-print 'The hopefuls are:', str(hopefuls)
-print 'Hopeful, but mind the pe are:', str(hopefuls_mind_pe)
-print 'The undervalued stocks are:', str(undervalued_stocks)
-print 'Ignoring dividends, these warrant a look:', str(ignore_dividend_stocks)
+        print 'Finding quote for %s' % quote
+
+        # First thing we want is some basic information on the quote.
+        details = ystockquote.get_by_id_list(quote, [
+            'p',  # 0 Previous close
+            'l1',  # 1 Last trade price
+            'o',  # 2 Today's open
+            'w1',  # 3 Today's value change
+            'e7',  # 4 EPS - Current year
+            'e9',  # 5 EPS - Next year
+            'r',  # 6 PE ratio
+            'y',  # 7 Dividend yield
+        ])
+
+        # Set up some variables that are used throughout
+        last_trade = float(details[1])
+
+        print 'Prev Close: %s' % details[0], 'Today Open: $%s' % details[2], 'Last Trade: $%s' % last_trade
+        print 'Today\'s Change: %s' % details[3].split(' ')[2].rstrip('"')
+
+        # Get the past ~50 BIZ days of trading.
+        volatility = get_volatility(quote)
+        print 'Volatility: %s%%' % (volatility)
+        VOLATILITY = True if volatility > DESIRED_VOLATILITY_RATE else False
+
+        # Get Ben grahams formula
+        try:
+            eps = float(details[4])
+        except ValueError:
+            # Likely returned N/A
+            eps = 0
+        try:
+            next_eps = float(details[5])
+        except ValueError:
+            next_eps = 0
+
+        # IV stands for Intrinsic Value.  What the stock is possibly really worth.
+        iv = ben_graham(eps, next_eps, AAA_BOND_RATE)
+
+        if iv != 0:
+            # Get the relative intrinsic value, RIV
+            riv = round(iv / last_trade, 4)
+            print 'RIV: %s' % riv  # We want RIV to be better than 1.00, which would mean it's undervalued.
+            RIV_RATIO = True if riv > DESIRED_RIV_RATE else False
+        else:
+            riv = 0
+
+        # Add to our list if applicable
+        if riv > 1.0:
+            undervalued_stocks.append(quote)
+
+        # Check out the PE ratio.
+        try:
+            pe = float(details[6])
+            print 'P/E ratio: %s' % pe
+            PE_RATIO = True if pe <= DESIRED_PE_RATIO else False
+        except ValueError:
+            pe = 'Check Research'
+            pass
+
+        # Check out the dividends on this babe
+        try:
+            div_yield = float(details[7])
+            print 'Dividend yield: %s%%' % div_yield
+            DIVIDEND_YIELD = True if div_yield >= DESIRED_DIVIDEND_RATE else False 
+        except ValueError:
+            print 'Dividend yield: %s' % details[7]
+
+        if DIVIDEND_YIELD and RIV_RATIO and VOLATILITY and PE_RATIO:
+            print '***** Check out: %s *****' % quote
+            hopefuls.append(quote)
+        elif DIVIDEND_YIELD and RIV_RATIO and VOLATILITY:
+            print '***** Check out: %s, mind the PE (%s) *****' % (quote, pe)
+            hopefuls_mind_pe.append(quote)
+        elif RIV_RATIO and VOLATILITY:
+            ignore_dividend_stocks.append(quote)
+
+        print ''
+
+    print 'The hopefuls are:', str(hopefuls)
+    print 'Hopeful, but mind the pe are:', str(hopefuls_mind_pe)
+    print 'The undervalued stocks are:', str(undervalued_stocks)
+    print 'Ignoring dividends, these warrant a look:', str(ignore_dividend_stocks)
+
+if __name__ == '__main__':
+    process()
+
